443d1c9fdad461c31afa61d73344d9e9e75d8cf5
Demiantcev <kirushakotik@gmail.com>
2023-07-19 10:59:23 +0300

    Initial Commit

---
+++ AstonWeatherApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstonWeatherApplication
+//
+//  Created by Кирилл Демьянцев on 19.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
